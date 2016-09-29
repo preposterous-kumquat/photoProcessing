48cddf6bc6e1d88dfe06a622bb0cc09101b5a276
Gamerlazer <gamerlazer@gmail.com>
2016-09-28 21:28:18 -0700

    Send post to savedPhoto upon photo processing completion

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,9 +2,11 @@ const multer = require('./multer.js');
 const photoUpload = require('../photoProcessers/photoUpload');
 
 module.exports = (app, express) => {
-  app.post('/test', (req, res) => {
-    console.log('requested', req.body);
+  app.post('/test', multer, (req, res) => {
+    console.log('requested', req);
+    console.log('body', req.body);
+    console.log('file', req.file);
     res.send('here');
   });
-  app.post('/photoProcessor/upload', multer, photoUpload);
+  app.post('/photoProcessor/upload/:id', multer, photoUpload);
 };
