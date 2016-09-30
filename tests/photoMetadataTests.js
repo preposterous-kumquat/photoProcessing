0ce3594a76769333a33000548a297dd14012b48d
Gamerlazer <gamerlazer@gmail.com>
2016-09-29 17:29:21 -0700

    Add more tests

--- tests/photoMetadataTests.js
+++ tests/photoMetadataTests.js
@@ -7,19 +7,21 @@ describe('photoMetadata function', () => {
   it('photoMetadata should be a function', () =>{
     expect(photoMetadata).to.be.a('function');
   })
-  it('photoMetadata should return a null if no GPS data present', () => {
+  it('photoMetadata should return a null if no GPS data present', (done) => {
     console.log(`${__dirname}/test-photos/HongKong_icecream.jpg`)
     photoMetadata(`${__dirname}/test-photos/HongKong_icecream.jpg`)
       .then((gps) => {
-        expect(gps).to.equal(null);    
+        expect(gps).to.equal(null);
+        done();    
       });
   });
-  xit('photoMetadata should return create directories', () => {
-    let photoId = 12345;
-    let charBlock = 3;
-    let path = photoMetadata(photoId, charBlock)
-    expect(path).to.contain('/')
-  })
+  it('photoMetadata should if present should be an object', (done) => {
+    photoMetadata(`${__dirname}/test-photos/Marce.jpg`)
+      .then((gps) => {
+        expect(gps).to.be.a('object');    
+        done()
+      });
+  });
   xit('photoMetadata should return the correct path', () => {
     let photoId = 12345;
     let charBlock = 3;
@@ -27,3 +29,4 @@ describe('photoMetadata function', () => {
     expect(path).to.equal('000/000/012/345/000000012345.jpg')
   })
 })
+
